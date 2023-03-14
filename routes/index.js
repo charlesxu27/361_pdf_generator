4cb4c06fb31a84b4f04c0b6b1c631f228045bcc3
charlesxu27 <charlesxu27@gmail.com>
2023-03-13 22:35:43 -0700

    Integrate with openAPI

--- routes/index.js
+++ routes/index.js
@@ -1,48 +1,86 @@
-const express = require('express')
-const PDFDocument = require('pdfkit');
+l\const express = require("express");
+const PDFDocument = require("pdfkit");
 
-const router = express.Router()
+const router = express.Router();
 
-router.post('/pdf', async (req, res) => {
+const API_KEY = process.env.MY_API_KEY;
+const API_ENDPOINT = process.env.MY_API_ENDPOINT;
 
-    const doc = new PDFDocument({ font: 'Helvetica', compress: false });
+async function getCoverLetter(chatPrompt) {
+  const response = await axios.post(
+    API_ENDPOINT,
+    {
+      prompt: chatPrompt,
+      stop: "\n",
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+        "Content-Type": "applicaiton/json",
+      },
+    }
+  );
 
-    doc.pipe(res)
+  const { choices } = response.data;
+  return choices[0].text;
+}
 
-    let date = new Date();
-    let options = { month: 'long', day: 'numeric', year: 'numeric' };
-    let formattedDate = date.toLocaleDateString('en-US', options);
+router.post("/pdf", async (req, res) => {
+  const coverLetterPrompt =
+    "Given the following resume: \n" +
+    req.body.resumeText +
+    "\n and the following role: " +
+    req.body.jobTitle +
+    "at the company: " +
+    req.body.companyName +
+    "with the job description: " +
+    req.body.jobDescription +
+    "write a cover letter without any salutations or closing remarks.";
 
-    let username = req.body.userName
-    let user_address = req.body.userAddress
-    let hiring_manager = req.body.hiringManagerName
-    let company_address = req.body.companyAddress
-    let letter_content = req.body.letterContent
+  const doc = new PDFDocument({ font: "Helvetica", compress: false });
 
-    doc.fontSize(12);
-    doc.text(formattedDate, { align: 'left' });
-    doc.moveDown();
-    doc.text(username, { align: 'left' });
-    doc.text(user_address, { align: 'left' });
-    doc.moveDown();
-    doc.text(hiring_manager, { align: 'left' });
-    doc.text(company_address, { align: 'left' });
-    doc.moveDown();
-    doc.text(letter_content, { align: 'left' });
-    doc.moveDown();
-    doc.text("Sincerely,", { align: 'left'});
-    doc.moveDown(); 
-    doc.text(username, {align: 'left'});
+  doc.pipe(res);
 
-    // Finalize PDF file
-    doc.end();
+  let date = new Date();
+  let options = { month: "long", day: "numeric", year: "numeric" };
+  let formattedDate = date.toLocaleDateString("en-US", options);
 
-    res.writeHead(200, {
-        'Content-Type': 'application/pdf',
-        'Content-Disposition': `attachment;filename=coverletter.pdf`,
-      });
+  let username = req.body.firstName + req.body.lastName;
+  let hiring_manager = req.body.hiringManagerName;
+
+  let company_address =
+    req.body.address1 +
+    "\n" +
+    req.body.city +
+    "\n" +
+    req.body.state +
+    req.body.zipcode;
+
+  let content = await getCoverLetter(coverLetterPrompt);
+
+  doc.fontSize(12);
+  doc.text(formattedDate, { align: "left" });
+  doc.moveDown();
+  doc.text(hiring_manager, { align: "left" });
+  doc.text(company_address, { align: "left" });
+  doc.moveDown();
+  doc.moveDown();
+  doc.text("Dear " + hiring_manager + ", ", { align: "left" });
+  doc.moveDown();
+  doc.text(content, { align: "left" });
+  doc.moveDown();
+  doc.text("Sincerely,", { align: "left" });
+  doc.moveDown();
+  doc.text(username, { align: "left" });
+
+  // Finalize PDF file
+  doc.end();
+
+  res.writeHead(200, {
+    "Content-Type": "application/pdf",
+    "Content-Disposition": `attachment;filename=coverletter.pdf`,
+  });
 });
-
 
 module.exports = router;
 
